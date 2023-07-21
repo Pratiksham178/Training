e2f97e2df77c1162986cd6858c50e428683e1b7a
Pratiksham178 <mfsi.pratiksham@gmail.com>
2023-07-21 11:54:28 +0530

    Pushed C# files with mail part

---
+++ AssignmentC#/ADO_NET/ADO_NET/Class1.cs
@@ -0,0 +1,131 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_NET
+{
+    class Program
+    {
+        static string connectionString = "server=127.0.0.1;uid=root;pwd=mindfire;database=profileapplication";
+
+        public static void inserttable()
+        {
+            using (var con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+                //MySqlCommand sde = new MySqlCommand("select * from country_table;", con);
+                Console.WriteLine("Enter the country id");
+                var countryId = Console.ReadLine();
+                Console.WriteLine("Enter the country name");
+                var country_name = Console.ReadLine();
+                Console.WriteLine("Enter the country abbr");
+                var country_abbr = Console.ReadLine();
+                using (MySqlCommand sde = new MySqlCommand($"Insert into country_table values('{countryId}','{country_name}','{country_abbr}')", con))
+                {
+                    var result = sde.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        Console.WriteLine("Data Inserted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data Not Inserted");
+                    }
+                };
+                con.Close();
+                return;
+            }
+        }
+        public static void update()
+        {
+            using (var con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+                Console.WriteLine("Enter the country id of country you want to update");
+                var countryIdchange = Console.ReadLine();
+                using (MySqlCommand select = new MySqlCommand($"select * from country_table where countryId={countryIdchange};", con))
+                {
+                    DataTable dataTable = new DataTable();
+                    using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(select))
+                    {
+                        dataAdapter.Fill(dataTable);
+                    }
+                    foreach (DataRow num in dataTable.Rows)
+                    {
+                        Console.WriteLine($"{num["countryId"]} , {num["country_name"]}");
+                    }
+                }
+                Console.WriteLine("Enter the new country name");
+                var country_namechange = Console.ReadLine();
+                Console.WriteLine("Enter the new country abbr");
+                var country_abbrchange = Console.ReadLine();
+                using (MySqlCommand update = new MySqlCommand($"update country_table set country_name='{country_namechange}',country_abbr='{country_abbrchange}' where countryId={countryIdchange};", con))
+                {
+                    var result = update.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        Console.WriteLine("Data Inserted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data Not Inserted");
+                    }
+                }
+                con.Close();
+                return;
+            }
+        }
+        public static void showtable(MySqlConnection con)
+        {
+                using (MySqlCommand select = new MySqlCommand("select * from country_table;", con))
+                {
+                    DataTable dataTable = new DataTable();
+                    using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(select))
+                    {
+                        dataAdapter.Fill(dataTable);
+                    }
+                    foreach (DataRow num in dataTable.Rows)
+                    {
+                        Console.WriteLine($"{num["countryId"]} , {num["country_name"]}");
+                    }
+                }
+                Console.WriteLine("");
+                Console.ReadLine();
+                return;
+            
+        }
+        public static void Main()
+        {
+            int decision = 0;
+            var con = new MySqlConnection(connectionString);
+            con.Open();
+            while (decision != 4)
+            {
+                Console.WriteLine("Enter what you want \n1.Show table\n2.Insert Table\n3.Update Table\n4.Exit");
+                decision = Int32.Parse(Console.ReadLine());
+                switch (decision)
+                {
+                    case 1:
+                        showtable(con);
+                        break;
+                    case 2:
+                        inserttable();
+                        break;
+                    case 3:
+                        update();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            con.Close();
+
+
+        }
+    }
+}
--- AssignmentC#/Mail/Mail/Program.cs
+++ AssignmentC#/Mail/Mail/Program.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using System.Configuration;
 
 class Program
 {
+    public static string GetPath(string keyname)
+    {
+        string value;
+        value = ConfigurationManager.AppSettings.Get(keyname);
+
+        return value;
+    }
     static void Main(string[] args)
     {
         MailMessage mailMessage = new MailMessage();
-        mailMessage.From = new MailAddress("liti624@gmail.com");
-        mailMessage.To.Add("mfsi.pratiksham@gmail.com");
+        mailMessage.From = new MailAddress(GetPath("mailfrom"));
+        mailMessage.To.Add(GetPath("mailto"));
         mailMessage.Subject = "Subject";
         mailMessage.IsBodyHtml = true;
         //mailMessage.Body = "This is test email";
-        mailMessage.Body = "<h1 style='color: red;font-variant:small-caps'>Welcome Abode</h1>";
-        Attachment attachment = new Attachment("D:\\Projects\\Assignments\\AssignmentC#\\Logs\\19-07-2023.txt");
+        mailMessage.Body = "<h1 style='color: red;font-variant:small-caps'>Welcome Aboard!</h1>";
+        Attachment attachment = new Attachment(GetPath("attachment"));
         mailMessage.Attachments.Add(attachment);
 
         SmtpClient smtpClient = new SmtpClient();
-        smtpClient.Host = "sandbox.smtp.mailtrap.io";
-        smtpClient.Port = 2525;
+        smtpClient.Host = GetPath("host");
+        smtpClient.Port = Int32.Parse(GetPath("port"));
         smtpClient.UseDefaultCredentials = false;
-        smtpClient.Credentials = new NetworkCredential("e3232103225356", "7b2c68df49aaa3");
+        smtpClient.Credentials = new NetworkCredential(GetPath("username"), GetPath("password"));
         smtpClient.EnableSsl = true;
 
         try
